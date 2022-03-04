852cbb674576bf1b6a7ee3c71d709cc3869c26d7
Weiller Carvalho <engweiller@hotmail.com>
2022-03-04 00:52:51 -0300

    endlogictes

--- logictest/logictests.py
+++ logictest/logictests.py
@@ -508,12 +508,30 @@ for i in range(0,M):
 '''
 # Cada linha
 
-n = int(input('Qual a ordem da matriz?'))
-while n > 10:
-    print(f"\n Não é possível aceitar números maiores que 10.")
-    n = int(input('Qual a ordem da matriz?'))
-matriz = [[0 for x ]]
+'''
+n = int(input("Qual a ordem da matriz? "))
+matriz = [[0 for x in range(n)] for x in range(n)]
+maiorlinha = []
 
+for i in range(n):
+	for j in range(n):
+		matriz[i][j] = int(input(f"Elemento [{i},{j}]: "))
+
+for i in range(n):
+	maior = matriz[i][0]
+	for j in range(1, n):
+		if maior < matriz[i][j]:
+			maior = matriz[i][j]
+
+	maiorlinha.append(maior)
+
+
+print("MAIOR ELEMENTO DE CADA LINHA:")
+
+for i in range(n):
+	print(maiorlinha[i])
+'''
+#End
 
 
 
